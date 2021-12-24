b5c25513adf2f01a6c94da4528a327c2d7c20f68
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-24 17:21:14 -0300

    At this point we are not using the password encoder in the manager component, we are delegating this task to the provider component

--- src/main/java/com/github/pedroluiznogueira/securityapi/security/MySecurityConfig.java
+++ src/main/java/com/github/pedroluiznogueira/securityapi/security/MySecurityConfig.java
@@ -16,10 +16,6 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Lazy
-    @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
-
     @Autowired
     private MyAuthenticationProvider authenticationProvider;
 
@@ -33,10 +29,5 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();
         http.authorizeRequests().anyRequest().authenticated();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }

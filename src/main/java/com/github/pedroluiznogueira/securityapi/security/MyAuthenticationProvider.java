bdf7034e1707140e636da866317544685a59b2b0
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-24 17:07:21 -0300

    The AuthenticationProvider will be responsible to receive the user object, access his username and password, compare it to the needed credentials and return a token or an exception

--- src/main/java/com/github/pedroluiznogueira/securityapi/security/MyAuthenticationProvider.java
+++ src/main/java/com/github/pedroluiznogueira/securityapi/security/MyAuthenticationProvider.java
@@ -1,14 +1,26 @@
 package com.github.pedroluiznogueira.securityapi.security;
 
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+
+import java.util.Arrays;
 
 public class MyAuthenticationProvider implements AuthenticationProvider {
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return null;
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        if (username.equals("tom") && password.equals("cruise")) {
+            // third parameter as a Collection as one of the authorizations
+            return new UsernamePasswordAuthenticationToken(username, password, Arrays.asList());
+        } else {
+            throw new BadCredentialsException("Invalid username of password");
+        }
     }
 
     @Override

f88a543761f422530eb4084b81de9d924f781525
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-24 16:09:23 -0300

    Setting up wich kind of security we will use, form based, http etc...

--- src/main/java/com/github/pedroluiznogueira/securityapi/security/MySecurityConfig.java
+++ src/main/java/com/github/pedroluiznogueira/securityapi/security/MySecurityConfig.java
@@ -2,6 +2,7 @@ package com.github.pedroluiznogueira.securityapi.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 // this class has 3 important methods
@@ -11,5 +12,10 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     // customizing AuthenticationManager component
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    }
+
+    // wich kind of security
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
     }
 }

9923b287d8605287e67a68c25c92e40270807e53
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-24 16:07:34 -0300

    Setting up AuthenticationManager component

--- src/main/java/com/github/pedroluiznogueira/securityapi/security/MySecurityConfig.java
+++ src/main/java/com/github/pedroluiznogueira/securityapi/security/MySecurityConfig.java
@@ -1,9 +1,15 @@
 package com.github.pedroluiznogueira.securityapi.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 // this class has 3 important methods
+@Configuration
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
-
+    // customizing AuthenticationManager component
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    }
 }

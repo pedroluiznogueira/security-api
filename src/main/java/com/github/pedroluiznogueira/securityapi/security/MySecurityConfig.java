0599c5484f376bee9c1bb22193cde18daeff7994
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-24 16:18:15 -0300

    Implementing UserDetailsService for the provider component

--- src/main/java/com/github/pedroluiznogueira/securityapi/security/MySecurityConfig.java
+++ src/main/java/com/github/pedroluiznogueira/securityapi/security/MySecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 // this class has 3 important methods
 @Configuration
@@ -12,9 +15,20 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     // customizing AuthenticationManager component
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // authentication provider component
+
+        // UserDetailsService
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+
+        // wich user to UserDetailsService to use
+        UserDetails user = User.withUsername("tom").password("cruise").build();
+        userDetailsService.createUser(user);
+
+        // telling UserDetailsService about password encoder
+        auth.userDetailsService(userDetailsService);
     }
 
-    // wich kind of security
+    // wich type of security
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();

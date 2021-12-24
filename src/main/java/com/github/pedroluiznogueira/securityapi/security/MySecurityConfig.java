9576eac65d55ee8ca40939421f970b64a34d5bea
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-24 17:23:00 -0300

    If we configure as the type of security to use, formLogin(), we see a html form if we access the localhost:8080 in browser

--- src/main/java/com/github/pedroluiznogueira/securityapi/security/MySecurityConfig.java
+++ src/main/java/com/github/pedroluiznogueira/securityapi/security/MySecurityConfig.java
@@ -27,7 +27,7 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
+        http.formLogin();
         http.authorizeRequests().anyRequest().authenticated();
     }
 }

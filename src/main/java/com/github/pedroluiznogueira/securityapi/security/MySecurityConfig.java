e0eca7c643eb085f18ff180cf96f97ffeaa2aa88
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-24 16:04:17 -0300

    Creating MySecurityConfig class, wich extends WebSecurityConfigurerAdapter

---
+++ src/main/java/com/github/pedroluiznogueira/securityapi/security/MySecurityConfig.java
@@ -0,0 +1,9 @@
+package com.github.pedroluiznogueira.securityapi.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+// this class has 3 important methods
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+}

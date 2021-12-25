9e5075d38b9c76cf45fd062f63565fe9b61757f0
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-25 20:05:08 -0300

    Implementing necessary dependency to use SCryptPasswordEncoder()

--- src/test/java/com/github/pedroluiznogueira/securityapi/SecurityApiApplicationTests.java
+++ src/test/java/com/github/pedroluiznogueira/securityapi/SecurityApiApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 @SpringBootTest
 class SecurityApiApplicationTests {
@@ -12,5 +13,6 @@ class SecurityApiApplicationTests {
 	void testPasswordEncoder() {
 		System.out.println(new BCryptPasswordEncoder().encode("password"));
 		System.out.println(new Pbkdf2PasswordEncoder().encode("password"));
+		System.out.println(new SCryptPasswordEncoder().encode("password"));
 	}
 }

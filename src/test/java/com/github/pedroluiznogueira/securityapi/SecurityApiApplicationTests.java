b9f69fd74e8113b8a504733e1e768c8b4200829f
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-25 20:02:00 -0300

    Encoding with Pbkdf2PasswordEncoder() results in a much more longer password

--- src/test/java/com/github/pedroluiznogueira/securityapi/SecurityApiApplicationTests.java
+++ src/test/java/com/github/pedroluiznogueira/securityapi/SecurityApiApplicationTests.java
@@ -3,6 +3,7 @@ package com.github.pedroluiznogueira.securityapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 @SpringBootTest
 class SecurityApiApplicationTests {
@@ -10,5 +11,6 @@ class SecurityApiApplicationTests {
 	@Test
 	void testPasswordEncoder() {
 		System.out.println(new BCryptPasswordEncoder().encode("password"));
+		System.out.println(new Pbkdf2PasswordEncoder().encode("password"));
 	}
 }

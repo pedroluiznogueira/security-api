3a2908109c994b99776cd78c0ff8266ffa60d339
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-25 20:11:22 -0300

    DelegatingPasswordEncoder is useful if you need to use different types of encoders along the project

--- src/test/java/com/github/pedroluiznogueira/securityapi/SecurityApiApplicationTests.java
+++ src/test/java/com/github/pedroluiznogueira/securityapi/SecurityApiApplicationTests.java
@@ -3,8 +3,13 @@ package com.github.pedroluiznogueira.securityapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 class SecurityApiApplicationTests {
@@ -13,7 +18,13 @@ class SecurityApiApplicationTests {
 	void testPasswordEncoder() {
 		System.out.println(new BCryptPasswordEncoder().encode("password"));
 		System.out.println(new Pbkdf2PasswordEncoder().encode("password"));
-		// way more secure
 		System.out.println(new SCryptPasswordEncoder().encode("password"));
+
+		// delating password encoder
+		Map<String, PasswordEncoder> encoders = new HashMap<>();
+		encoders.put("bcrypt", new BCryptPasswordEncoder());
+		encoders.put("scrypt", new SCryptPasswordEncoder());
+
+		new DelegatingPasswordEncoder("scrypt", encoders).encode("password");
 	}
 }

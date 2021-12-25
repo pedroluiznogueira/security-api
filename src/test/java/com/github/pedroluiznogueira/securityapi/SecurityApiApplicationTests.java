de6c8f8fbe68a1fb26a17f47135d4baa7aa5fb32
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-25 19:59:48 -0300

    Implementing a test to test encode() method of BCryptPasswordEncoder

--- src/test/java/com/github/pedroluiznogueira/securityapi/SecurityApiApplicationTests.java
+++ src/test/java/com/github/pedroluiznogueira/securityapi/SecurityApiApplicationTests.java
@@ -2,12 +2,13 @@ package com.github.pedroluiznogueira.securityapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class SecurityApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testPasswordEncoder() {
+		System.out.println(new BCryptPasswordEncoder().encode("password"));
 	}
-
 }

b522d5f67097964d8c61b90dee2d002fc9ab760b
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-28 00:06:08 -0300

    Implemented TokenService, wich is responsible to build the jwt

--- src/main/java/com/github/pedroluiznogueira/securityapi/security/service/TokenService.java
+++ src/main/java/com/github/pedroluiznogueira/securityapi/security/service/TokenService.java
@@ -1,5 +1,6 @@
 package com.github.pedroluiznogueira.securityapi.security.service;
 
+import com.github.pedroluiznogueira.securityapi.model.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -8,8 +9,30 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
+import java.util.Date;
+
 @Service
 public class TokenService {
 
+    // env variable
+    @Value("${jwt.expiration}")
+    private String expiration;
+
+    // env variable
+    @Value("${jwt.secret}")
+    private String secret;
+
+    // generate a token based on username and password
+    public String generateToken(Authentication authentication) {
+
+        User usuario = (User) authentication.getPrincipal();
+
+        Date now = new Date();
+        Date exp = new Date(now.getTime() + Long.parseLong(expiration));
+
+        // build jwt
+        return Jwts.builder().setIssuer("IRS").setSubject(usuario.getId().toString()).setIssuedAt(new Date())
+                .setExpiration(exp).signWith(SignatureAlgorithm.HS256, secret).compact();
+    }
 
 }
